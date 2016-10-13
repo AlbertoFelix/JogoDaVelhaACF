7a24338313d542396bead21115e8d4a3fda2c6b2
Filipe Fernandes <filipeferibeiro@outlook.com>
2016-10-13 10:30:13 -0300

    Adicionado a ciração da matriz do jogo e criada a função que permite da o print nela

--- JogoDaVelhaACF.py
+++ JogoDaVelhaACF.py
@@ -1,1 +1,20 @@
 '''Jogo da velha criado por Alberto, Clara e Filipe Fernandes'''
+
+'''Variaveis'''
+MatrizJogo = []
+
+'''Criacao da Matriz do jogo'''
+for i in range(3):
+	linha = []
+	for j in range(3):
+		linha.append('[   ]')
+	MatrizJogo.append(linha)
+
+'''Funcoes'''
+def printMatriz():
+	for i in range(len(MatrizJogo)):
+		for j in range(len(MatrizJogo[i])):
+			print(MatrizJogo[i][j], end= ' ')
+	print('\n')
+
+printMatriz()
